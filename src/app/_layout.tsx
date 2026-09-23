122ee40cf1a2182a0e7b34e51394384e153af6a5
koleks92_NEXT <koleks92@gmail.com>
2026-09-23 14:32:07 +0200

    feat: Added fonts and splashscreen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,25 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    DMSerifDisplay: require("@/assets/fonts/DMSerifDisplay-Regular.ttf"),
+    Outfit: require("@/assets/fonts/Outfit-VariableFont_wght.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <Stack />;
 }
