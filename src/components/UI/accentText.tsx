c2eb4e9210da1caa8c2a9daeefff6f0ddadd49d5
koleks92_NEXT <koleks92@gmail.com>
2026-09-24 13:57:25 +0200

    feat: AccentText component

---
+++ src/components/UI/accentText.tsx
@@ -0,0 +1,18 @@
+import { Colors } from "@/constants/colors";
+import { rfs } from "@/util/responsiveFontSizing";
+import { PropsWithChildren } from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function AccentText({ children }: PropsWithChildren) {
+  return <Text style={styles.root}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  root: {
+    fontFamily: "Outfit",
+    color: Colors.fontMuted,
+    fontWeight: 700,
+    fontSize: rfs(12),
+    letterSpacing: 3,
+  },
+});
