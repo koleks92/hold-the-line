fa8506c10b1d474365859891e3915871d76186fa
koleks92_NEXT <koleks92@gmail.com>
2026-09-24 14:36:37 +0200

    feat: SafeAreProvider + fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,13 +2,20 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     DMSerifDisplay: require("@/assets/fonts/DMSerifDisplay-Regular.ttf"),
-    Outfit: require("@/assets/fonts/Outfit-VariableFont_wght.ttf"),
+    Outfit100: require("@/assets/fonts/Outfit-Thin.ttf"),
+    Outfit200: require("@/assets/fonts/Outfit-ExtraLight.ttf"),
+    Outfit300: require("@/assets/fonts/Outfit-Light.ttf"),
+    Outfit400: require("@/assets/fonts/Outfit-Regular.ttf"),
+    Outfit500: require("@/assets/fonts/Outfit-Medium.ttf"),
+    Outfit700: require("@/assets/fonts/Outfit-Bold.ttf"),
+    Outfit800: require("@/assets/fonts/Outfit-ExtraBold.ttf"),
   });
 
   useEffect(() => {
@@ -21,5 +28,13 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeAreaProvider>
+  );
 }
