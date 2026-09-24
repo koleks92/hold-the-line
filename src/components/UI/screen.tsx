5fb23b08e0a66e8fdd34382bb243d8e4ae69515a
koleks92_NEXT <koleks92@gmail.com>
2026-09-24 13:57:58 +0200

    feat: align center and padding

--- src/components/UI/screen.tsx
+++ src/components/UI/screen.tsx
@@ -5,15 +5,24 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 type ScreenProps = PropsWithChildren<{
   title?: string;
+  padding?: boolean;
 }>;
 
-export default function Screen({ children, title }: ScreenProps) {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+export default function Screen({ children, title, padding }: ScreenProps) {
+  return (
+    <SafeAreaView style={[styles.container, padding && styles.padding]}>
+      {children}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.bg,
+    alignItems: "center",
+  },
+  padding: {
+    padding: "15%",
   },
 });
