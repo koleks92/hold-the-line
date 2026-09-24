eb677c5a9b331881503e252a5dd2b696a117c170
koleks92_NEXT <koleks92@gmail.com>
2026-09-24 14:41:15 +0200

    feat: MainButton component

--- src/components/UI/buttonMain.tsx
+++ src/components/UI/buttonMain.tsx
@@ -1,21 +1,47 @@
+import { Colors } from "@/constants/colors";
+import { Sizes } from "@/constants/sizes";
+import { rfs } from "@/util/responsiveFontSizing";
 import { PropsWithChildren } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 
 type ButtonMainProps = PropsWithChildren<{
+  gold?: boolean;
   onPress: () => void;
 }>;
 
-export default function ButtonMain({ children, onPress }: ButtonMainProps) {
+export default function ButtonMain({
+  children,
+  gold,
+  onPress,
+}: ButtonMainProps) {
   return (
-    <Pressable style={styles.root} onPress={() => onPress()}>
-      <Text>{children}</Text>
+    <Pressable
+      style={[styles.root, gold && styles.gold]}
+      onPress={() => onPress()}
+    >
+      <Text style={styles.text}>{children}</Text>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   root: {
+    width: "45%",
+    height: Sizes.scrW * 0.14,
+    backgroundColor: Colors.card,
+    borderColor: Colors.border,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontFamily: "Outfit500",
+    fontWeight: 500,
+    fontSize: rfs(13),
+    color: Colors.font,
+  },
+  gold: {
+    backgroundColor: Colors.primary,
     width: "100%",
-    height: 50,
   },
 });
