fc07995bd21427ce61a9c59f9b994da413ed0f5b
koleks92_NEXT <koleks92@gmail.com>
2026-09-24 09:52:51 +0200

    feat: Colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  bg: "#14181a",
+  card: "#1d2326",
+  secondary: "#242c30",
+  muted: "#1f2629",
+  fg: "#edeae2",
+  fgMuted: "#6a7e78",
+  fgSub: "#a8b8b0",
+  primary: "#d4a843",
+  primaryFg: "#14181a",
+  accent: "#e05c3a",
+  border: "#2e3a3f",
+};
